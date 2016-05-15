63397ec1e0e8253e9fd22e259bc8e3dca8127cbf
Bruno Wego <brunowego@gmail.com>
2016-05-14 21:21:08 -0300

    chore(npm): add more automatisation

--- grunt/browserSync.js
+++ grunt/browserSync.js
@@ -1,6 +1,13 @@
 module.exports = {
+  options: {
+    background: true,
+  },
   server: {
     options: {
+      files: [
+        './client/*.html',
+        './tmp/styles/*.css',
+      ],
       server: {
         baseDir: [
           './client',
---
+++ grunt/watch.js
@@ -0,0 +1,6 @@
+module.exports = {
+  styles: {
+    files: './client/styles/*.scss',
+    tasks: 'sass:server',
+  },
+};
